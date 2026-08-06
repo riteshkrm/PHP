46ebc04eae08d3c492baa8a9c0105b353c970c62
riteshkrm <rtskrm@gmail.com>
2026-08-07 00:45:00 +0530

    expression completed

--- Expression/expression.php
+++ Expression/expression.php
@@ -1,5 +1,30 @@
 <?php
 // What is expression?
 // Anything that has value, that called expression..
+// Can't use php reserved words :- $this, $class, $function
 
-echo "Learing php ";
+//  (\n) use for new line
+echo "Learing php \n";
+
+// Variable Expression
+$name = "Ritesh Sharma";
+
+
+// Concatenation Operator ( string . Dot)
+echo "Name :- "  . $name . "\n";
+
+// Arithmetic Operator (+, -. *, /, %)
+
+$pizzas = 3;
+$slicesPerPizzas = 8;
+
+$totalSlices = $pizzas * $slicesPerPizzas;
+
+echo "Total pizza slices :- " . $totalSlices .  "\n";
+
+// Boolean value
+
+$isHungry = false ;
+echo "Hungry?";
+echo $isHungry ?  "yes" :  "no"; // ternary operator
+echo "\n";
