e9971cffdd3f89b1b22817e5885fac2369a9a98b
riteshkrm <rtskrm@gmail.com>
2026-08-07 01:21:03 +0530

    Data Type Completed

---
+++ DataTypes/types.php
@@ -0,0 +1,8 @@
+<?php
+
+$isStudent = 10;
+
+var_dump($isStudent);  
+// var_dump() is a PHP debugging function used to inspect the type, value, and size of variables. It helps you understand exactly what a variable contains while your code is running.
+
+echo "Students are :- $isStudent \n";
